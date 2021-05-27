984c81b8116df3fd518e47a233eda5e27513c23e
eugene131 <80297365+eugene131@users.noreply.github.com>
2021-05-27 15:20:30 +0900

    Add files via upload

--- hwrsearch.py
+++ hwrsearch.py
@@ -12,7 +12,7 @@ def get_key(val,dic):
 	for key, value in dic.items():
 		if val ==value:
 			return key
-			
+
 def hfilter(s):
 	return re.sub(u'[^ \.\,\?\!\u3130-\u318f\uac00-\ud7a3]+','',s)
 
@@ -23,51 +23,53 @@ def make_index(es, index_name):
 
 es_host="127.0.0.1"
 es_port="9200"
-
+D = {}
 if __name__ == '__main__':
-	
-	es=Elasticsearch([{'host':es_host, 'port':es_port}], timeout=30)
 
-	es.info()	
-	index_name = 'goods'
-	make_index(es, index_name)
-	
-	req = requests.get('https://en.wikipedia.org/wiki/Web_crawler')
-	html = req.text
-	soup = BeautifulSoup(html, 'html.parser')
+	es = Elasticsearch([{'host':es_host, 'port':es_port}], timeout=30)
+#	index_name = 'goods'
+#	make_index(es, index_name)
+
+	req = requests.get(u'https://en.wikipedia.org/wiki/Web_crawler')
+	soup = BeautifulSoup(req.content, 'html.parser')
 	lists = soup.find_all('div',class_='mw-parser-output')
-	
+
 #	text1 = "".join(text for text in lists.find_all(text=True) if text.parent.name !="span")
 #	content=soup.get_text('\n',strip=True)
 #	print(content)
-	text=[]
+
+#               수정한부분 
+	p={}
 	for list1 in lists:
-	#	if list1.parent.name != "article":
-		Tex=list1.text.replace("\n","")
-		text.extend(Tex.split())
-#		print(Tex.split())
+		text = list1.get_text()
+		arr = text.split()
+		print (arr)
 		
-	line1=[]
-	for i in text:
-		line1.extend(i.split())
-#	print(line1)
-	p={}	
-	for i in line1:
-		if i in p:
-			p[i] += 1
-		else:
-			p[i] = 1
-	print(p)
-	j=0
-	for i in line1:
-		temp=(list)(p.values())
-#	print(temp)
+		for i in arr:
+			if i in p:
+				p[i]+=1
+			else:
+				p[i]=1
+		j=0
+		for i in arr:
+			temp=list(p.values())
 	temp.sort()
 	temp.reverse()
-	es.index(index=index_name, doc_type='string', body=p)
-	es.indices.refresh(index=index_name)
+	for i in range(int(3)):
+			print(str(get_key(temp[i],p).ljust(10)),str(temp[i]).rjust(10))
+	j=0
 #	print(temp)
-#	for i in range(3):
-#		print(str(get_key(temp[i],p).ljust(10)),str(temp[i]).rjust(10))	
-		
-		
+	num=1
+	for i,j in p.items():
+		e={"word" : i, "num":j}
+		es.index(index='good', doc_type='string',id=num, body=e)
+		num+=1
+	
+	
+	
+	
+	
+	
+	
+	
+	
